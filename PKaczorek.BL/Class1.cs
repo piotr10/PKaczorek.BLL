49dd462ec2584768647e64a24af78df49ed68d90
Piotr Czerski <piotr.czerski10@gmail.com>
2019-11-17 11:00:11 +0100

    add properties to class PKaczorek.Bl

--- PKaczorek.BL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PKaczorek.BL
-{
-    public class Class1
-    {
-    }
-}
---
+++ PKaczorek.BL/PKlient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PKaczorek.BL
+{
+    public class PKlient
+    {
+        #region Właściwości
+        private string _nazwisko;
+        public string  Nazwisko
+        {
+            get
+            {
+                //tutaj mozemy dodac dowolny kod
+                return _nazwisko;
+            }
+            set
+            {
+                //tutaj mozemy dodac dowolny kod
+                _nazwisko = value;
+            }
+        }
+        public string Imie { get; set; }
+        public string Email { get; set; }
+        public int KlientId { get; private set; }
+
+        public string ImieNazwisko
+        {
+            get { return Imie + ", " + Nazwisko; }
+        }
+
+        #endregion
+
+        #region Konstruktory
+
+        #endregion
+
+        #region Metody
+
+        #endregion
+    }
+}
